1e7386dd8a458a338273b611a2c27ee0630e3d62
Aron Felippe Kerkhoven <aronkerk8@gmail.com>
2023-03-23 20:56:53 -0300

    Update Plataform Game

--- Video_tutorial_DaFluffyPotato/Plataform/main.py
+++ Video_tutorial_DaFluffyPotato/Plataform/main.py
@@ -6,7 +6,7 @@ pygame.init()
 # Sound ------------------------------------------------------------------
 
 # Variaveis ------------------------------------------------------------------
-SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+WINDOW_SIZE = (1280,720)
 FPS = 60
 
 # Player movement
@@ -15,7 +15,7 @@ MOVE_LEFT = False
 GRAVITY = 0
 
 
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+SCREEN = pygame.display.set_mode(WINDOW_SIZE, 0 ,32)
 pygame.display.set_caption("Template Pygame init")
 
 DISPLAY = pygame.Surface((300, 200))
@@ -25,7 +25,7 @@ DISPLAY = pygame.Surface((300, 200))
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREEN = (124,252,0)
-
+BLUE = (146, 244, 255)
 
 
 
@@ -57,14 +57,10 @@ def load_map(path):
 
 game_map = load_map(os.path.join(os.path.dirname(__file__), 'map'))
 
+TRUE_SCROLL = [0, 0]
 
-
-MAP_WIDTH = len(game_map[0]) * TILE_SIZE
-MAP_HEIGHT = len(game_map) * TILE_SIZE
-
-# Calculate the offset required to center the map
-OFFSET_X = (300 - MAP_WIDTH) // 2
-OFFSET_Y = (200 - MAP_HEIGHT) // 2
+# Aqui eu to criando diversos tipo de objetos, o primeiro valor é o valor do parallax, e o outro valor é o valor do X, Y, tamanho do rect.
+background_objects = [[0.25,[120,10,70,400]],[0.25,[280,30,40,400]],[0.5,[30,40,40,400]],[0.5,[130,90,100,400]],[0.5,[300,80,120,400]]]
 
 # Funções ------------------------------------------------------------------
 
@@ -105,24 +101,45 @@ def move(rect, movement, tiles):
 run = True
 clock = pygame.time.Clock()
 
-PLAYER_RECT = pygame.Rect(50, 50, PLAYER.get_width(), PLAYER.get_height())
+PLAYER_RECT = pygame.Rect(100,100,5,13)
 
 air_timer = 0
 
 while run:
-    DISPLAY.fill(WHITE)
+    DISPLAY.fill(BLUE)
     
-    clock.tick(FPS)
+
+
+    # Câmera para seguir o player, o conceito de câmera é basicamente dar um SCROLL para todos os assets, ou seja, fazer eles andarem para fora da tela.
+    TRUE_SCROLL[0] += (PLAYER_RECT.x - TRUE_SCROLL[0] - 152)/20
+    TRUE_SCROLL[1] += (PLAYER_RECT.y - TRUE_SCROLL[1] - 106)/20
+
+    SCROLL = TRUE_SCROLL.copy()
+    SCROLL[0] = int(SCROLL[0])
+    SCROLL[1] = int(SCROLL[1])
+
+
+    # Criando um rect aleatório no meio da tela com cor verde
+    pygame.draw.rect(DISPLAY, GREEN, pygame.Rect(0, 120, 300, 80))
+
+    # Aqui eu to passando por todos os objetos e criando um rect com cada uma das informações, a multiplicação serve para saber qual vai ser q velocidade do parallax de cada um.
+    for background_object in background_objects:
+        obj_rect = pygame.Rect(background_object[1][0] - SCROLL[0] * background_object[0], background_object[1][1] - SCROLL[1] * background_object[0], background_object[1][2], background_object[1][3])
+        if background_object[0] == 0.5:
+            pygame.draw.rect(DISPLAY,(14,222,150),obj_rect)
+        else:
+            pygame.draw.rect(DISPLAY,(9,91,85),obj_rect)
+
 
     tile_rect = []
-    for y, row in enumerate(game_map):
-        for x, tile in enumerate(row):
+    for y, layer in enumerate(game_map):
+        for x, tile in enumerate(layer):
             if tile == '1':
-                DISPLAY.blit(DIRT_IMG, (x * TILE_SIZE + OFFSET_X, y * TILE_SIZE + OFFSET_Y))
+                DISPLAY.blit(DIRT_IMG, (x * TILE_SIZE - SCROLL[0], y * TILE_SIZE - SCROLL[1]))
             if tile == '2':
-                DISPLAY.blit(GRASS_IMG, (x * TILE_SIZE + OFFSET_X, y * TILE_SIZE + OFFSET_Y))
+                DISPLAY.blit(GRASS_IMG, (x * TILE_SIZE -  SCROLL[0], y * TILE_SIZE - SCROLL[1]))
             if tile != '0':
-                tile_rect.append(pygame.Rect(x * TILE_SIZE + OFFSET_X, y * TILE_SIZE + OFFSET_Y, TILE_SIZE, TILE_SIZE))
+                tile_rect.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
     
     
@@ -145,7 +162,7 @@ while run:
     else:
         air_timer += 1
 
-    DISPLAY.blit(PLAYER, (PLAYER_RECT.x, PLAYER_RECT.y))
+    DISPLAY.blit(PLAYER, (PLAYER_RECT.x - SCROLL[0], PLAYER_RECT.y - SCROLL[1]))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -167,8 +184,8 @@ while run:
                 MOVE_RIGHT = False
         
 
-    surf = pygame.transform.scale(DISPLAY, (SCREEN_WIDTH, SCREEN_HEIGHT))
-    SCREEN.blit(surf, (0, 0))
+    SCREEN.blit(pygame.transform.scale(DISPLAY, WINDOW_SIZE), (0, 0))
     pygame.display.flip() # update display
+    clock.tick(FPS)
     
 pygame.quit()
